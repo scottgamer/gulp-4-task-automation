67be809542cb7af423698f58c19b86bd8ce75a0e
richard munoz <richosojason@msn.com>
2020-10-14 23:59:27 -0500

    set up gulp uglifyu to minify js

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const sass = require("gulp-sass");
 const sourcemaps = require("gulp-sourcemaps");
 const browserSync = require("browser-sync").create();
 const cssNano = require("gulp-cssnano");
+const uglify = require("gulp-uglify");
 
 gulp.task("hello", (done) => {
   console.log("hello gulp!");
@@ -28,6 +29,13 @@ gulp.task("sass", () => {
     .pipe(gulp.dest("./dist/css"));
 });
 
+gulp.task("javascript", () => {
+  return gulp
+    .src(["./src/js/**/*.js"])
+    .pipe(uglify())
+    .pipe(gulp.dest("./dist/js"));
+});
+
 gulp.task("watch", () => {
   browserSync.init({
     server: {
@@ -37,6 +45,9 @@ gulp.task("watch", () => {
   });
 
   gulp
-    .watch(["./src/sass/**/*.scss", "**/*.html"], gulp.series(["sass"]))
+    .watch(
+      ["./src/sass/**/*.scss", "**/*.html", "./src/js/**/*.js"],
+      gulp.series(["sass", "javascript"])
+    )
     .on("change", browserSync.reload);
 });
